90dbc2da64bc537d7ed6bd3e3bdac2b2a2ee8b35
Lewis Cowper <lewis.cowper@googlemail.com>
2018-02-27 21:46:33 +0100

    isolate lots of things from the main function, and split it all apart into other functions, for the most part

--- main.go
+++ main.go
@@ -47,7 +47,7 @@ func computeMd5(filePath string) (string, error) {
 
 	h := md5.New()
 	if _, err := io.Copy(h, file); err != nil {
-		log.Fatal(err)
+		return result, err
 	}
 
 	checksum := h.Sum(nil)
@@ -69,42 +69,55 @@ func createCheckSum(filename string, hash string) error {
 	return nil
 }
 
-func main() {
-	// get the current date for timestamp purposes
-	current_time := time.Now().Unix()
-	filename := fmt.Sprintf("%v-%v", "backup", current_time)
-
-	// set up the output file
-	file, err := os.Create(fmt.Sprintf("%v.tar.gz", filename))
+func createArchive(files []string, archiveFilename string) error {
+	file, err := os.Create(archiveFilename)
 	if err != nil {
-		log.Fatalln(err)
+		return err
 	}
-	// set up the gzip writer
-	gw := gzip.NewWriter(file)
-	tw := tar.NewWriter(gw)
-	// grab the paths that need to be added in
-	paths := os.Args[1:]
+	defer file.Close()
 
-	// add each file as needed into the current tar archive
-	for i := range paths {
-		if err := addFile(tw, paths[i]); err != nil {
-			log.Fatalln(err)
+	gw := gzip.NewWriter(file)
+	defer gw.Close()
+	tw := tar.NewWriter(gw)
+	defer tw.Close()
+
+	for i := range files {
+		if err := addFile(tw, files[i]); err != nil {
+			return err
 		}
 	}
 
-	// close our writers in reverse order before calculating checksum
-	tw.Close()
-	gw.Close()
-	file.Close()
+	return nil
+}
 
-	if hash, err := computeMd5(fmt.Sprintf("%v.tar.gz", filename)); err != nil {
+func getFilenames(prefix string) (archiveFilename string, checksumFilename string) {
+	currentTime := time.Now().Unix()
+	filename := fmt.Sprintf("%v-%v", prefix, currentTime)
+
+	archiveFilename = fmt.Sprintf("%v.tar.gz", filename)
+	checksumFilename = fmt.Sprintf("%v.md5", filename)
+
+	return archiveFilename, checksumFilename
+}
+
+func main() {
+	files := os.Args[1:]
+
+	archiveFilename, checksumFilename := getFilenames("backup")
+
+	err := createArchive(files, archiveFilename)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if hash, err := computeMd5(archiveFilename); err != nil {
 		fmt.Printf("Error getting md5 sum: %v", err)
 	} else {
-		err := createCheckSum(fmt.Sprintf("%v.md5", filename), hash)
+		err := createCheckSum(checksumFilename, hash)
 		if err != nil {
 			log.Fatalln(err)
 		}
 
-		log.Printf("Backup created with filename %v and checksum %v", filename, hash)
+		log.Printf("Backup created at: %v", archiveFilename)
 	}
 }
